8a460ec39d9b51123c6eddfb0b6a62185531985e
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-11-23 17:49:19 +0200

    Add project files.

---
+++ SPD_311_AsyncAwait/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SPD_311_AsyncAwait
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static Random random = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //async -  allow method to use await keywords
+        //await - wait task without freeze
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //int value = GenerateValue();
+            //Task<int> task=   Task.Run(GenerateValue);
+            //task.Wait();//freeze
+            //list.Items.Add(task.Result);//freeze
+            /// 
+            //int value = await task;
+            //int value = await Task.Run(GenerateValue);
+            //list.Items.Add(value);
+            //list.Items.Add(await Task.Run(GenerateValue));
+            //MessageBox.Show("Complete in button");
+           
+
+            list.Items.Add(await GenerateValueAsync());
+            ///
+            //File.Copy()
+        }
+
+        int GenerateValue()
+        {
+            Thread.Sleep(random.Next(5000));
+            //MessageBox.Show("Complete");
+            return random.Next(1000);
+        }
+        Task<int> GenerateValueAsync()
+        {
+            return Task.Run(() =>
+            {
+                Thread.Sleep(random.Next(5000));
+                //MessageBox.Show("Complete");
+                return random.Next(1000);
+            });
+         
+        }
+    }
+}
